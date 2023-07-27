4c519661b5e314e7bf643646a9c76fdfa8abde42
Aryan Anand <97431499+aryans08@users.noreply.github.com>
2023-07-27 16:33:01 +0530

    Update dbconnect.php

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -3,8 +3,8 @@
     $servername = "localhost"; 
     // $username = "tsunami"; 
     // $password = "munia2003";
-    $username = "root"; 
-    $password = "";
+    $username = "id21082625_aryan"; 
+    $password = "id21082625_organ_donation";
    
     $database = "Organ_Donation";
      $conn = mysqli_connect($servername, 
